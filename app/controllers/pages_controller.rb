1bfde772d22d437cc11a11a7d917dd9e6a5d2dd0
Izzy Brown <izzybrown@hotmail.co.uk>
2019-01-09 13:14:21 +0000

    added tech

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,4 +13,15 @@ class PagesController < ApplicationController
     @description = json['items'][0]['snippet']['description']
     @date = json['items'][0]['snippet']['publishedAt']
   end
+
+  def tech
+    url = open("https://www.googleapis.com/youtube/v3/activities?part=snippet,contentDetails&channelId=UCrM7B7SL_g1edFOnmj-SDKg&key=#{ENV['API_KEY']}&maxResults=50").read
+    json = JSON.parse(url)
+
+    #select json items that you want to display
+    @items = json['items']
+    @image_id = json['items'][0]['contentDetails']['upload']['videoId']
+    @description = json['items'][0]['snippet']['description']
+    @date = json['items'][0]['snippet']['publishedAt']
+  end
 end
